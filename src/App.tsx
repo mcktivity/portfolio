7122066da76966ee042e250589fbac47dbf52a91
mcktivity <mcktivity@gmail.com>
2020-10-01 19:52:37 +0200

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,13 @@ import React from "react";
 import "./css/site.scss";
 
 import About from "./views/About";
-import Navbar from "./components/NavBar/Navbar";
 import Banner from "./views/Banner";
 import Logo from "./views/Logo";
-import SocialMedia from "./views/SocialMedia";
-import Page from "./components/Page/Page";
-import Container from "./components/Container/Container";
-import Menu from "./components/Menu/Menu";
-import MenuItem from "./components/Menu/MenuItem";
 import Navigation from "./views/Navigation";
 import Arbeid from "./views/Arbeid";
 import Teknologi from "./views/Teknologi";
 
 function App() {
-  // Under arbeid
-  //
-  // const hour = () => {
-  //   if (new Date().getHours() > 16) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // };
-  // const [isNight, setNight] = useState(true);
-
   return (
     <main className="App">
       <Logo />
@@ -36,34 +19,6 @@ function App() {
         <Arbeid />
         <Teknologi />
       </section>
-
-      {/* 
-      <Navbar>
-        <p>
-          <a
-            href="https://docs.google.com/document/d/1w7j3xbgvnlE5JDx7bVEQ4pzm30AxFr-n07321r5k5Kk/edit?usp=sharing"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Les CVen
-          </a>
-          Fullt cv på Google Docs. Åpnes i nytt vindu.
-          <a
-            href="https://github.com/mcktivity/portfolio"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Src kode
-          </a>
-          Source kode i Github. Åpnes i nytt vindu.
-        </p>
-        <SocialMedia />
-        <p>nye og spennende prosjekter kommer senere.</p>
-      </Navbar>
-      <Banner />
-      <section className="about">
-        <About />
-      </section> */}
     </main>
   );
 }
--- src/components/Container/Container.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default ({ children }: { children: any }) => (
-  <main className="main-container" id="container">
-    {children}
-  </main>
-);
